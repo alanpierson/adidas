2d4990f1730e955a1587997b7adbeef4e44fffeb
Alan <alanjpierson@gmail.com>
2018-01-23 16:25:39 -0500

    General Improvements

---
+++ Adidas/Assets/Scripts/FramePosition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FramePosition : MonoBehaviour {
+
+    [SerializeField]
+    int m_AnimationFrames;
+
+    [SerializeField]
+    int m_MaxPosition;
+
+    private static int mDivisor;
+
+    public static int mFramePosition;
+
+	void Start () {
+        mDivisor = m_MaxPosition / m_AnimationFrames;
+	}
+	
+	void Update () {
+        mFramePosition = -((int)GetComponent<RectTransform>().offsetMin.x / mDivisor);
+        //Debug.Log(mFramePosition);
+	}
+}
---
+++ Adidas/Assets/Scripts/ImageSequenceTextureArray.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSequenceTextureArray : MonoBehaviour
+{
+    //An array of Objects that stores the results of the Resources.LoadAll() method  
+    private Object[] objects;
+    //Each returned object is converted to a Sprite and stored in this array  
+    private Sprite[] mSprites;
+    //With this Material object, a reference to the game object Material can be stored  
+    private Image mImage;
+
+    void Awake()
+    {
+        //Get a reference to the Image of the game object this script is attached to  
+        mImage = this.GetComponent<Image>();
+    }
+
+    void Start()
+    {
+        //Load all textures found on the Sequence folder, that is placed inside the resources folder  
+        this.objects = Resources.LoadAll("Sequence", typeof(Sprite));
+
+        //Initialize the array of sprites with the same size as the objects array  
+        this.mSprites = new Sprite[objects.Length];
+
+        //Cast each Object to Sprite and store the result inside the Sprites array  
+        for (int i = 0; i < objects.Length; i++)
+        {
+            this.mSprites[i] = (Sprite)this.objects[i];
+        }
+    }
+
+    void Update()
+    {
+        //Set the image's sprite to the current value of the FramePosition variable  
+        mImage.sprite = mSprites[FramePosition.mFramePosition];
+    }
+}
